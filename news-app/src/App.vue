c3bd9f2eeae258d99bd34be6aa9bc0ee9b036f30
CodePen9 <1061268907@qq.com>
2020-06-25 22:00:51 +0800

    vue-cli

---
+++ news-app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <div style="width:1080px; margin:0 auto;">
+      <Banner />
+    </div>
+  </div>
+</template>
+<script>
+import Banner from "./components/Banner.vue"
+export default {
+  components:{
+    Banner,
+  }
+};
+</script>
